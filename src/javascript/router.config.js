f8ce63d7140089f2e0fa24350daff666a7810239
hooman mousavi <hoomanmousavi77@gmail.com>
2022-12-14 01:23:58 +0330

    netlify.toml added

--- src/javascript/router.config.js
+++ src/javascript/router.config.js
@@ -1,4 +1,4 @@
-import {createRouter,createWebHashHistory,createWebHistory} from "vue-router";
+import {createRouter,createWebHashHistory} from "vue-router";
 
 //////////// main content
 import Main from '../javascript/Main.vue'
@@ -383,7 +383,7 @@ const routes=[
 
 
 const router=createRouter({
-    history:createWebHistory(),
+    history:createWebHashHistory(),
     routes,
     scrollBehavior(to,from,savedPosition){
         if(savedPosition){
