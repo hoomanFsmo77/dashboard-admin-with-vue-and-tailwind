bd90237de289fe8c1281f38b0a7c0810e8201a6e
hooman mousavi <hoomanmousavi77@gmail.com>
2022-12-15 18:52:30 +0330

    ecommerce >> order_details  completed

--- src/javascript/components/Header/NavbarButton.vue
+++ src/javascript/components/Header/NavbarButton.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="cursor-pointer sm:p-0 p-0.5 px-0.65 sm:!ring-0 transition-all" :class="{'ring-4 ring-secondary-indigo/50 rounded-4':isFocus}">
+  <div  class="cursor-pointer sm:p-0 p-0.5 px-0.65 sm:!ring-0 transition-all" :class="{'ring-4 ring-secondary-indigo/50 rounded-4':isFocus}">
     <button
         v-on="active!==undefined ? sidebarButtonHandler : buttonHandlers"
         :class="icon"
+        v-bind="$attrs"
         class="text-secondary-gray text-1.5 cursor-pointer hover:text-gray-700 transition-all relative"
     >
+      <slot name="content"></slot>
       <span  :class="badge" class="badge "><slot/></span>
     </button>
   </div>
--- src/javascript/pages/Ecommerce/Order-details.vue
+++ src/javascript/pages/Ecommerce/Order-details.vue
@@ -1,13 +1,188 @@
 <template>
-  Order-details
+  <div class="container-fluid">
+    <row>
+      <column col="12">
+        <Breadcrumb :pages="[{name:'home',link:'Main'},{name:'Orders',link: 'Orders'},{name:'Order #25646'}]"/>
+        <div class="flex flex-col md:flex-row justify-between md:items-center md:mb-0 mb-1">
+          <h1 class="text-primary-dark uppercase tracking-widest !px-0 mt-1.2">Orders #25646</h1>
+          <div class="flex">
+            <span class="category-card flex items-center bg-green-200 text-green-600 text-[0.7rem] md:text-[0.9rem] mr-1">
+                <i class="bi bi-circle-fill !text-[0.4rem] mx-0.25"></i>
+              Paid
+            </span>
+            <span class="category-card flex items-center bg-orange-200 text-orange-600 text-[0.7rem] md:text-[0.9rem] ">
+              <i class="bi bi-circle-fill !text-[0.4rem] mx-0.25"></i>
+              Being Processed
+            </span>
+          </div>
+
+        </div>
+
+        <div class="flex justify-between md:flex-row flex-col items-center">
+          <div class="flex gap-1 items-center">
+            <AppLink to="#" class="text-[0.8rem] text-gray-500 hover:underline">
+              <i class="fa-solid fa-download"></i>
+              Export
+            </AppLink>
+            <AppLink to="#" class="text-[0.8rem] text-gray-500 hover:underline">
+              <i class="fa-solid fa-print"></i>
+              Print
+            </AppLink>
+            <div class="relative">
+              <NavbarButton
+                  class="!flex !gap-0.65 !text-[0.9rem] !font-500 !items-center !text-gray-500 !cursor-pointer !p-0.5 !rounded-4"
+                  @show="toggleFocus"
+                  :active="undefined" badge="!w-0 !h-0"
+              >
+                <template v-slot:content>
+                  <i class="bi bi-plus-circle-fill"></i>
+                  More actions
+                  <i class="bi bi-caret-down-fill"></i>
+                </template>
+
+              </NavbarButton>
+
+              <Transition name="show">
+                <Dropdown v-if="show" top="54px" width="150px">
+                  <li class="dropdown-hover !text-[0.9rem] !text-gray-700">
+                    <AppLink to="#!">
+                      Action
+                    </AppLink>
+                  </li>
+                  <li class=" dropdown-hover !text-[0.9rem] !text-gray-700">
+                    <AppLink to="#!">
+                      Another action
+                    </AppLink>
+                  </li>
+                  <li class=" dropdown-hover !text-[0.9rem] !text-gray-700">
+                    <AppLink to="#!">
+                      Something else
+                    </AppLink>
+                  </li>
+                </Dropdown>
+              </Transition>
+
+            </div>
+          </div>
+          <div class="text-[0.8rem] text-gray-600 flex gap-0.5">
+            <i class="bi bi-calendar-fill !text-[0.7rem]"></i>
+            Jun 22, 2021 — 10:25 AM
+          </div>
+        </div>
+
+      </column>
+    </row>
+    <row>
+      <column col="12" md="8">
+        <row class="!mt-0 !pt-0">
+          <column col="12" class="!pt-0">
+            <Card title="Order details" body-class="overflow-hidden !px-0">
+              <div class="overflow-x-auto w-0 min-w-full">
+                <table class="w-full" >
+                  <thead class="bg-gray-100 border-b-2 border-b-primary-dark">
+                  <tr >
+                    <th v-for="item in tableData10.th" colspan="1" class="uppercase tracking-wide text-[0.9rem] py-1.5 px-1 text-left" >
+                      <div >
+                        <span class="text-primary-dark uppercase">{{item}}</span>
+                      </div>
+                    </th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr class="table-hover " v-for="item in tableData10.td">
+                    <td class="p-1 px-1.3 ">
+                      <div class="flex items-center gap-1 md:flex-row flex-col">
+                        <img class="rounded-4" :src="item.img" width="100" alt="">
+                        <div>
+                          <span class="text-[0.9rem] font-600">{{item.name}}</span>
+                          <br>
+                          <span class="text-[0.8rem] text-gray-500 font-600">{{item.detail}}</span>
+                        </div>
+
+                      </div>
+
+                    </td>
+                    <td class="text-center">
+                      <span class="text-[0.9rem]">{{item.items}}</span>
+                    </td>
+                    <td class="text-center">
+                      <span class="text-[0.9rem]">${{item.price}}</span>
+                    </td>
+                    <td class="text-center">
+                      <span class="text-[0.9rem]">${{item.price * item.items}}</span>
+
+                    </td>
+                  </tr>
+                  </tbody>
+
+                </table>
+              </div>
+            </Card>
+          </column>
+        </row>
+        <row>
+          <column  col="12" md="6">
+            <Card title="Invoice address" body-class="!p-2">
+              <span class="text-[0.85rem] font-500 text-gray-600">
+                John Brown<br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br>
+                <span class="text-gray-700 font-700 flex items-center gap-1">Great Britain
+                  <img src="../../../assets/country/germany.svg" width="20" class="rounded-full" alt="">
+                </span>
+              </span>
+
+            </Card>
+          </column>
+          <column col="12" md="6">
+            <Card title="Shipping address" body-class="!p-2">
+              <span class="text-[0.85rem] font-500 text-gray-600">
+                John Brown<br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br>
+                <span class="text-gray-700 font-700 flex items-center gap-1">Great Britain
+                  <img src="../../../assets/country/germany.svg" width="20" class="rounded-full" alt="">
+                </span>
+              </span>
+            </Card>
+          </column>
+        </row>
+
+      </column>
+      <column col="12" md="4">
+          <Card title="Order details" body-class="!p-2">
+            <ul class="flex flex-col gap-1.5">
+              <li class="flex justify-between items-center text-[0.9rem] text-gray-500">
+                  <span>Products</span>
+                <span>$148.00</span>
+              </li>
+              <li class="flex justify-between items-center text-[0.9rem] text-gray-500">
+                  <span>Shipping Fee</span>
+                <span>$5.00</span>
+              </li>
+              <li class="flex justify-between items-center text-[0.9rem] text-gray-500">
+                  <span>Sales Tax</span>
+                <span>$7.00</span>
+              </li>
+              <li class="flex justify-between items-center text-[0.9rem] text-primary-dark font-600">
+                  <span>Total</span>
+                <span>$160.00</span>
+              </li>
+            </ul>
+
+          </Card>
+      </column>
+    </row>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Order-details"
-}
+<script setup>
+import Breadcrumb from '../../components/reusable/Breadcrumb.vue'
+import AppLink from '../../components/reusable/AppLink.vue'
+import useNavbar from "../../composables/useNavbar.js";
+import NavbarButton from '../../components/Header/NavbarButton.vue'
+import Dropdown from '../../components/Header/Dropdown.vue'
+import Card from '../../components/reusable/Card.vue'
+import {tableData5,tableData10} from "../../composables/Data/useTableData.js";
+import useMethods from "../../composables/useMethods.js";
+import ActivityCard from '../../components/reusable/ActivityCard.vue'
+const {toggleFocus,show}=useNavbar()
+const {bgClasses,textClasses}=useMethods()
 </script>
 
-<style scoped>
-
-</style>
