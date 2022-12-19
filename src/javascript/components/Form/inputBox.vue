7c688aa99de54df642465f9d5cbc39370118fdc1
hooman mousavi <hoomanmousavi77@gmail.com>
2022-12-19 20:54:12 +0330

    user >>> login page  completed

--- src/javascript/components/Form/inputBox.vue
+++ src/javascript/components/Form/inputBox.vue
@@ -1,25 +1,43 @@
 <template>
-  <label v-if="label" :for="id" class="block mb-0.5 text-gray-700 font-500 text-1">
-    {{label}}
-  </label>
-  <div :class="{'flex gap-0 items-center':icon}">
-    <i v-if="icon" class="py-[4px] px-[8px] bg-gray-700 text-white rounded-l-4" :class="icon"></i>
-    <input
-        :id="id"
-        v-bind="$attrs"
-        :type="type ?? 'text'"
-        :class="{'!rounded-l-[0px]':icon}"
-        class="input group-focus/item:ring-4 peer-focus:ring-4 "
-        :placeholder="placeholder"
-    />
-  </div>
-  <span v-if="hint" class="inline-block mt-0.5 text-gray-700 text-[0.8rem]">{{hint}}</span>
+  <template v-if="form===undefined">
+    <label v-if="label" :for="id" class="block mb-0.5 text-gray-700 font-500 text-1">
+      {{label}}
+    </label>
+    <div :class="{'flex gap-0 items-center':icon}">
+      <i v-if="icon" class="py-[4px] px-[8px] bg-gray-700 text-white rounded-l-4" :class="icon"></i>
+      <input
+          :id="id"
+          v-bind="$attrs"
+          :type="type ?? 'text'"
+          :class="{'!rounded-l-[0px]':icon}"
+          class="input group-focus/item:ring-4 peer-focus:ring-4 "
+          :placeholder="placeholder"
+      />
+    </div>
+    <span v-if="hint" class="inline-block mt-0.5 text-gray-700 text-[0.8rem]">{{hint}}</span>
+  </template>
+  <template v-else>
+    <div class="relative" v-bind="$attrs">
+      <input
+          :id="id"
+          :type="type ?? 'text'"
+          class="input peer !px-1 !pt-1.2 !pb-[0.8rem] group-focus/item:ring-4 peer-focus:ring-4 w-full"
+          required
+      />
+      <label  :for="id" class="absolute peer-focus:bottom-[59%] text-[0.9rem] text-primary-dark peer-focus:left-[3.5%] peer-focus:text-gray-600 peer-focus:text-0.75 transition-all left-[5%] bottom-[29%] text-gray-700 font-500 text-1 peer-valid:bottom-[59%] peer-valid:left-[3.5%] peer-valid:text-gray-600 peer-valid:text-0.75 ">
+        {{label}}
+      </label>
+
+    </div>
+  </template>
+
+
 </template>
 
 <script>
 export default {
   name: "inputBox",
-  props:['placeholder','label','id','hint','icon','type']
+  props:['placeholder','label','id','hint','icon','type','form']
 }
 </script>
 
--- src/javascript/pages/User/LoginPage.vue
+++ src/javascript/pages/User/LoginPage.vue
@@ -1,13 +1,58 @@
 <template>
-  Login page
+  <main class="bg-[#f8f9fa]  w-full">
+    <div class="container md:!px-3 sm:!px-2 !p-0.25">
+      <row class="!mt-0">
+        <column col="12" md="6">
+          <form @submit="$router.push({name:'Main'})">
+            <Card class="mt-1" :has-menu="false" title="Hooman Dashboard" body-class="!p-0">
+              <div class="p-2.5">
+                <div class="flex gap-0.5 items-center">
+                  <h3 class="text-primary-dark font-700">Hi, welcome back!</h3>
+                  <img src="../../../assets/hand.svg" width="30" alt="">
+                  <img src="../../../assets/hand.svg" width="30" alt="">
+                </div>
+                <p class="article !my-2 !text-[0.8rem]">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
+                </p>
+                <inputBox id="email" class="mt-2 mb-1 w-full" label="Email address"  form="float"/>
+                <inputBox id="password" label="Password" type="password"  form="float"/>
+                <div class="flex gap-0.5 items-center my-1">
+                  <input id="remember" type="checkbox" class="checkbox">
+                  <label for="remember" class=" text-[0.9rem] font-500 text-primary-dark">Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-primary-full ">Submit</button>
+              </div>
+              <div class="card-footer">
+                  <span class="text-[0.85rem] font-500 text-gray-500">Don't have an account? <router-link :to="{name:'Register'}" class="link">Register.</router-link> </span>
+              </div>
+            </Card>
+          </form>
+
+        </column>
+        <column col="12" md="6">
+          <div class="flex flex-col items-center justify-center">
+            <img src="../../../assets/money.svg" width="400" alt="">
+            <h2 class="text-indigo-700 font-800 text-center mt-1">
+              Start saving
+              <br>
+              your time & money
+            </h2>
+            <p class="text-gray-500 font-500 text-center mt-1 text-1.1">
+              One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed in
+            </p>
+
+          </div>
+
+        </column>
+      </row>
+    </div>
+  </main>
 </template>
 
-<script>
-export default {
-  name: "LoginPage"
-}
+<script setup>
+import Card from '../../components/reusable/Card.vue'
+import inputBox from '../../components/Form/inputBox.vue'
+
+
 </script>
 
-<style scoped>
-
-</style>
